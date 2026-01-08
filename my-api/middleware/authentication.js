89d15cd767d0473958c2037ba862291d91cd17a6
nyashachimutapira <chimutapiran@gmail.com>
2026-01-08 15:25:37 +0200

    commit

--- my-api/middleware/authentication.js
+++
@@ -1,8 +0,0 @@
-const isAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  return res.status(401).json({ error: 'Authentication required' });
-};
-
-module.exports = { isAuthenticated };
